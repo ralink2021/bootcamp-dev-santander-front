dd3a70181254ba019fd45ee9be4a000135967db7
Rafael Oliveira <seguranca_pentest@icloud.com>
2021-06-09 15:02:25 -0300

    ok

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class DashboardModule { }
